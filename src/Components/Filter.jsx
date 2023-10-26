c3c5407c851618dbd22056a5306c8f6801948704
Abhishek Singh <bobbyworld530@gmail.com>
2023-10-26 18:47:51 +0530

    fixed issue

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -122,6 +122,24 @@ const Filter = () => {
                             </div>
                           </div>
                         </div>
+
+                        {/*<!--Pagination--> */}
+                         
+                          <div className='mt-5'>
+                            <ul className='pagination justify-content-center'>
+                              <li className='page-item'>
+                                <a className='page-link' href="#"><span>{"<"}</span></a>
+                              </li>
+                              <li className='page-item'><a className='page-link' href="#">1</a></li>
+                              <li className='page-item'><a className='page-link' href="#">2</a></li>
+                              <li className='page-item'><a className='page-link' href="#">3</a></li>
+                              <li className='page-item'><a className='page-link' href="#">4</a></li>
+                              <li className='page-item'><a className='page-link' href="#">5</a></li>
+                              <li className='page-item'>
+                                <a className='page-link' href="#"><span>{">"}</span></a>
+                              </li>
+                            </ul>
+                          </div>
                        </div>
                </div>
 
